277265ea7fd100d59fa27b11b133d017180598c1
Nihit Verma <nihitv2003@gmail.com>
2023-09-28 14:46:18 +0530

    layout fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,9 @@
+"use client";
 
+import React from 'react';
 
-import {useState, useEffect} from 'react';
-import axios from 'axios';
+export default function Home(){
 
-import {message} from 'antd';
-
-import {cookies} from 'next/headers';
-
-
-
-// entire page will be loading from the server only
-
-export async function getUser(){
-  try{
-    const token = cookies().get("token")
-    const response = await axios.get('http://localhost:3000/api/users/currentuser' , {
-      headers:{
-        // we have to send the token in the cookie
-        Cookie: `token=${token?.value}`
-      }
-    });
-
-    return response.data.data;
-  
-  }
-  catch(error){
-    console.log(error);
-  }
 }
 
 
-
-
-export default async function Home() {
-
-  const user:any = await getUser();
-
-
-
-
-
-
-  return (
-    <div>
-      Orion Jobs
-      {
-        user?.name
-      }
-      <h1>
-              </h1>      
-    </div>
-  )
-}
--- src/components/LayoutProvider.tsx
+++ src/components/LayoutProvider.tsx
@@ -1,30 +1,139 @@
 "use client";
-import React from 'react'
+import React, { useState } from "react";
+import { ConfigProvider } from "antd";
+import { usePathname } from "next/navigation";
 
-
-import {ConfigProvider} from 'antd';
-
-interface ParentProps{
-    children: React.ReactNode
+interface ParentProps {
+  children: React.ReactNode;
 }
 
+const LayoutProvider: React.FC<ParentProps> = ({ children }) => {
+  const [menuItems, setMenuItems] = useState([
+    {
+      name: "Home",
+      path: "/",
+      icon: "ri-home-7-line",
+    },
+    {
+      name: "Profile",
+      path: "/profile",
+      icon: "ri-shield-user-line",
+    },
+    {
+      name: "Applications",
+      path: "/applications",
+      icon: "ri-file-list-2-line",
+    },
+    {
+      name: "Settings",
+      path: "/settings",
+      icon: "ri-settings-2-line",
+    },
+    {
+      name: "Saved",
+      path: "/saved",
+      icon: "ri-save-line",
+    },
+  ]);
 
-const LayoutProvider : React.FC<ParentProps> = ({children}) => {
+  const pathName = usePathname();
+
+
+  const [IsSidebarExpanded, setIsSidebarExpanded] = useState(true);
+
+
   return (
-    <html lang='en'>
-        <body>
-            <ConfigProvider
-            theme={{
-                token:{
-                    colorPrimary: '#213555',
-                }
-            }}
-            >
-                {children}
-            </ConfigProvider>
-        </body>
-    </html>
-  )
-}
+    <html lang="en">
+      <head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/remixicon@2.2.0/fonts/remixicon.css"
+          rel="stylesheet"
+        />
+      </head>
 
-export default LayoutProvider
+      <body>
+        <ConfigProvider
+          theme={{
+            token: {
+              colorPrimary: "#213555",
+            },
+          }}
+        >
+          {/* IF THE ROUTE IS PUBLIC (register, login) then dont show the pages */}
+
+          {pathName === "/login" || pathName === "/register" ? (
+            <>
+              <div>{children}</div>
+            </>
+          ) : (
+            <>
+              <div className="layout-parent">
+                <div className="sidebar">
+                  <div className="logo">
+                    {
+                        IsSidebarExpanded && <h1>Orion Jobs</h1>
+                    }
+
+
+                    {
+                        !IsSidebarExpanded && (
+                            <i className="ri-menu-line" onClick={()=>{
+                                setIsSidebarExpanded(!IsSidebarExpanded)
+                            }} ></i>
+                        )
+                    }
+
+                    {
+                        IsSidebarExpanded && (
+                            <i className="ri-close-line" onClick={()=>{
+                                setIsSidebarExpanded(!IsSidebarExpanded)
+                            }} ></i>
+                        )
+                    }
+
+                    
+                  </div>
+                  <div>
+                    {menuItems.map((item, index) => {
+                        const isActive = pathName === item.path;
+                      return (
+
+                        <div className={`menu-item ${isActive ? "active-menu-item" : "" }`}>
+
+                            <i className={item.icon}>
+                            </i>
+                            
+                            <span>{IsSidebarExpanded && item.name}</span>
+                          
+                        </div>
+                      );
+                    })}
+                  </div>
+                  <div className="user-info">
+                    {
+                        IsSidebarExpanded && (
+                            <div className="flex flex-col">
+                                <span>UserName</span>
+                                <span>UserEmail</span>
+                            </div>
+                        )
+                    }
+                    
+                    <div>
+                        <i className="ri-logout-box-r-line"></i>
+                    </div>
+
+                  </div>
+                </div>
+
+                <div className="body">{children}</div>
+              </div>
+            </>
+          )}
+        </ConfigProvider>
+      </body>
+    </html>
+  );
+};
+
+export default LayoutProvider;
